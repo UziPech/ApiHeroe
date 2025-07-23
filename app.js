bd3e48e5e187c4bf389575e3272808f4f2f48be5
UzielIsaac <uzielisaac28@gmail.com>
2025-07-23 05:03:29 -0600

    CORS definitivo: instalado paquete cors + configuración correcta para Vercel

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
@@ -167,18 +168,13 @@ app.get('/docs', (req, res) => {
 app.use(express.json());
 app.use(express.static('public'));
 
-// Configuración CORS para Vercel
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-});
+// Configuración CORS para Vercel - VERSIÓN CORRECTA
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+    credentials: false
+}));
 
 // Ruta de prueba
 app.get('/', (req, res) => {
