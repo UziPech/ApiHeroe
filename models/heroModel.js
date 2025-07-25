e04d3571fef593a13f3897dfd1b617cf990359bc
UzielIsaac <uzielisaac28@gmail.com>
2025-07-24 21:34:26 -0600

    correccion de personajes duplicados

--- models/heroModel.js
+++ models/heroModel.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 
+
 const heroSchema = new mongoose.Schema({
   id: { type: Number, required: true, unique: true },
-  name: { type: String, required: true },
-  alias: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
+  alias: { type: String, required: true, unique: true },
   power: { type: Number, required: true },
   city: { type: String, default: '' },
   team: { type: String, default: '' }
