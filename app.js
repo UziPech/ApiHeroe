f7bdee1660340d184ad835332f8bdd616e88cc28
UzielIsaac <uzielisaac28@gmail.com>
2025-08-01 06:14:53 -0600

    EMERGENCY: Force CORS headers to fix deployment

--- app.js
+++ app.js
@@ -26,6 +26,19 @@ app.use(cors({
   credentials: true
 }));
 
+// Middleware adicional para forzar headers de CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 // Swagger configuration - SOLUCIÓN FINAL QUE FUNCIONA
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
