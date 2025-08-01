a30ec464b4e6f7efa239c198e60e3534dd5b3534
UzielIsaac <uzielisaac28@gmail.com>
2025-08-01 06:38:38 -0600

    EMERGENCY: Force CORS headers on all responses

--- app.js
+++ app.js
@@ -25,11 +25,22 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
   }
+  next();
+});
+
+// Middleware adicional para forzar headers en todas las respuestas
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  });
   next();
 });
 
